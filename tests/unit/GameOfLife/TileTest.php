e670e2bc0da175c8ad2fae7702fec27a2a2c672d
Simone Gentili <sensorario@gmail.com>
2018-11-13 11:19:08 +0100

    use more semantic named constructor

--- tests/unit/GameOfLife/TileTest.php
+++ tests/unit/GameOfLife/TileTest.php
@@ -11,4 +11,10 @@ final class TileTest extends TestCase
         $tile = Tile::create();
         $this->assertEquals('dead', $tile->status());
     }
+
+    public function testShouldBeCreatedUsingBetterNamedConstructor()
+    {
+        $deadTile = Tile::dead();
+        $this->assertEquals('dead', $deadTile->status());
+    }
 }
